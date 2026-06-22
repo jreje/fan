f61d7d951a10a21e283aaae34dcfdcf0fc26571b
Julianne Rejesus <jreje@ad.unc.edu>
2026-06-21 21:15:15 -0400

    Implement Fly Input Mapping

--- player/player.gd
+++ player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const FLY_VELOCITY = -100.0
 var screen_size
 
 func _ready():
@@ -10,6 +10,8 @@ func _ready():
 	
 func _process(delta):
 	position = position.clamp(Vector2.ZERO, screen_size)
+	if Input.is_action_pressed("fly"):
+		velocity.y = FLY_VELOCITY
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,8 +19,8 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
