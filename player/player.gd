4adf60cf6100190ef4e2e93ecab159232d37ed68
Julianne Rejesus <jreje@ad.unc.edu>
2026-06-22 01:06:24 -0400

    Implement Player Physics

--- player/player.gd
+++ player/player.gd
@@ -1,33 +1,30 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const FLY_VELOCITY = -100.0
+const FLY_VELOCITY = 100.0
 var screen_size
+var rotation_speed = 500.0
+var move_speed = 50.0
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	
-func _process(delta):
-	position = position.clamp(Vector2.ZERO, screen_size)
-	if Input.is_action_pressed("fly"):
-		velocity.y = FLY_VELOCITY
 
 func _physics_process(delta: float) -> void:
+	position = position.clamp(Vector2.ZERO, screen_size)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	# Tilting Mechanic
+	if Input.is_action_pressed("tilt_left"):
+		global_rotation_degrees -= rotation_speed * delta;
+	if Input.is_action_pressed("tilt_right"): 
+		global_rotation_degrees += rotation_speed * delta;
+	# Flying
+	if Input.is_action_pressed("fly"):
+		# TODO: Make sure if facing down, we have velocity and gravity compounded
+		velocity = Vector2(0, -100).rotated(rotation) * FLY_VELOCITY * delta
+		$AnimatedSprite2D.play("fly")
+	
+	if Input.is_action_just_released("fly"):
+		$AnimatedSprite2D.stop()
 	move_and_slide()
