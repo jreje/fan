4a4acbd9d4db13df222401ef30ec19bf94b36e44
Julianne Rejesus <jreje@ad.unc.edu>
2026-06-23 15:43:34 -0400

    Using Rigid Body Player

--- player/player.gd
+++ player/player.gd
@@ -1,40 +1,32 @@
-extends CharacterBody2D
+extends RigidBody2D
 
-const fly_speed = 100.0
+const THRUST_AMOUNT = 400.0
 var screen_size
 var rotation_speed = 500.0
-var move_speed = 50.0
-var direction 
 
 # TODO: For the future, top of fan should naturally tilt left/right making it harder to fly
 func _ready():
 	screen_size = get_viewport_rect().size
+	linear_damp = 5
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	
 	# Tilting Mechanic
-	direction = Input.get_axis("tilt_left", "tilt_right")
-	if direction:
-		rotation_degrees += direction * rotation_speed * delta
+	if Input.is_action_pressed("tilt_right"):
+		apply_torque(400)	
+	if Input.is_action_pressed("tilt_left"):
+		apply_torque(-400)	
+	# Tilting better, but now gets stuck into upside down position
 		
-	# Balancing
-	if global_rotation_degrees != 0:
-		rotation_speed = direction * rotation_speed * delta
-	
-	# Flying
+	# Flying. Referenced this Repo https://github.com/Weetbix/moon-mishap/blob/main/scripts/player.gd 
 	if Input.is_action_pressed("fly"):
-		# TODO: Make sure if facing down, we have velocity and gravity compounded
-		velocity = Vector2(0, -15).rotated(rotation) * fly_speed * delta
+		apply_force(Vector2(0, -15).rotated(rotation) * THRUST_AMOUNT)
 		$AnimatedSprite2D.play("fly")
 	
 	if Input.is_action_just_released("fly"):
-		velocity.x = direction * fly_speed * delta * 0.25
 		$AnimatedSprite2D.stop()
-	move_and_slide()
 	
 	# TODO: Make player snap to ground. May need to ask for help on this
 	# For now, have reduced screen size by half of the collisionshape's size
-	position = position.clamp(Vector2.ZERO, screen_size - ($CollisionShape2D.shape.size /2))
+	# position = position.clamp(Vector2.ZERO, screen_size - ($CollisionShape2D.shape.size /2))
+	
+	print(linear_velocity)
