5c6fca848d79eb65d65cd1e6f1112a4310684f0a
Julianne Rejesus <jreje@ad.unc.edu>
2026-06-25 19:46:28 -0400

    Move Minecraft Font

--- level/destination_block/destination_block.gd
+++ level/destination_block/destination_block.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 
+# TODO: Clean up and remove these functions
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
