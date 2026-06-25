d30e52e6cd0a9a40290fc4c36533442bc03fc622
Julianne Rejesus <jreje@ad.unc.edu>
2026-06-25 00:55:19 -0400

    Update Player Physics

--- player/player.gd
+++ player/player.gd
@@ -1,37 +1,50 @@
 extends RigidBody2D
 
-const THRUST_AMOUNT = 100.0
+var THRUST_AMOUNT = 40.0
 var screen_size
-var rotation_speed = 250.0
+var rotation_speed = 10.0
+var contact_count = 0
+var is_touching_boundary = false
 
-# TODO: For the future, top of fan should naturally tilt left/right making it harder to fly
+# TODO: Start scene with fan spinning
+# TODO: -- IMPORTANT -- Fix bug when fan is in corner
 func _ready():
 	screen_size = get_viewport_rect().size
 	linear_damp = 5
+	angular_damp = 5
+	contact_monitor = true
+	max_contacts_reported = 4
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
 
+# TODO: Simplify body_entered. Perhaps I can only use a body entered and the is in group property
+func _on_body_entered(body: Node) -> void:
+	is_touching_boundary = true
+
+func _on_body_exited(body: Node) -> void:
+	is_touching_boundary = false
+	
 func _physics_process(delta: float) -> void:
 	# Tilting Mechanic
 	if Input.is_action_pressed("tilt_right"):
-		apply_torque(400)	
+		apply_torque_impulse(rotation_speed)	
 	if Input.is_action_pressed("tilt_left"):
-		apply_torque(-400)	
-	# Tilting better, but now gets stuck into upside down position
+		apply_torque_impulse(-rotation_speed)	
+	# So players don't get stuck on the ground
+	if is_touching_boundary:
+		rotation_speed = 150
+	else:
+		rotation_speed = 10.0
 		
-	# Flying. Referenced this Repo https://github.com/Weetbix/moon-mishap/blob/main/scripts/player.gd 
+	# Flying
 	if Input.is_action_pressed("fly"):
-		apply_force(Vector2(0, -15).rotated(rotation) * THRUST_AMOUNT)
+		apply_impulse(Vector2(0, -1).rotated(rotation) * THRUST_AMOUNT)
 		$AnimatedSprite2D.play("fly")
 	
 	if Input.is_action_just_released("fly"):
 		$AnimatedSprite2D.stop()
 	
-	# TODO: Make player snap to ground. May need to ask for help on this
-	# For now, have reduced screen size by half of the collisionshape's size
-	# position = position.clamp(Vector2.ZERO, screen_size - ($CollisionShape2D.shape.size /2))
-	
-
-	
 	print(linear_velocity)
-	
-	
-		
+	print("ROTATION_SPEED: ", rotation_speed)
+	print("ANGULAR_DAMP: ", angular_damp)
+	print("THRUST_AMOUNT:", THRUST_AMOUNT)
