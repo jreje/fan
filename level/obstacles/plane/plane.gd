0c123a94594a96bc8c60ac87d0c717961d258cab
Jaydonisthe3rd <154110936+Jaydonisthe3rd@users.noreply.github.com>
2026-06-26 01:04:33 -0400

    moving plane!

--- level/obstacles/plane/plane.gd
+++ level/obstacles/plane/plane.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 @onready var target = $"../../Player"
 
 var speed = 100
+var direction = 1
+@onready var ray_cast_right = $Ray_Cast_Right
+@onready var ray_cast_left = $Ray_Cast_Left
+
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
@@ -10,6 +14,13 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 	#velocity = direction * speed
 	#look_at(target.position)
 	#move_and_slide()
+func _process(delta):
+	if ray_cast_right.is_colliding():
+		direction = -1
+	if ray_cast_left.is_colliding():
+		direction = 1
+		
+	position.x += direction * speed * delta
 
 
 	
--- main.gd
+++ main.gd
@@ -4,16 +4,14 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var plane = plane_scene.instantiate()
-	var plane_spawn_location = $PlanePath/PlaneSpawnLocation
-	plane_spawn_location.progress_ratio = randf()
 	
-	plane.position = plane_spawn_location.position
+	
+	
+	
 	
 	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
 	#plane.linear_velocity = velocity.rotated(direction)
-	
-	add_child(plane)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
